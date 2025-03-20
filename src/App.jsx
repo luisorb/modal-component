98dc6fbcd7366bc02f55a412a38e15cf4f93bb3d
Luis Rodolfo Rodriguez Betancourt <lrbetancourt@uuci.cu>
2025-03-20 12:14:29 -0400

    uploading modal component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ModalComponent from './components/ModalComponent'
+
+function App() {
+  
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <ModalComponent />
+    </div>
+  )
+}
+
+export default App
